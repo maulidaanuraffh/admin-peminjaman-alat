041c2b482c2328faceeed721a3c877f702114f8c
Maulida Nur Afifah <maulidaaa.nurafifah@gmail.com>
2024-03-06 22:44:31 +0700

    crud kategori

--- app/src/main/java/com/maulida/kinanti/adminpeminjamanalat/adapter/AdapterDataKategori.java
+++ app/src/main/java/com/maulida/kinanti/adminpeminjamanalat/adapter/AdapterDataKategori.java
@@ -1,4 +1,78 @@
 package com.maulida.kinanti.adminpeminjamanalat.adapter;
 
-public class AdapterDataKategori {
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.maulida.kinanti.adminpeminjamanalat.R;
+import com.maulida.kinanti.adminpeminjamanalat.ui.KategoriActivity;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class AdapterDataKategori extends RecyclerView.Adapter<AdapterDataKategori.HolderDataKtg> {
+
+    private final List<HashMap<String, String>> dataKtg;
+    private final KategoriActivity kategoriActivity;
+
+    public AdapterDataKategori(List<HashMap<String, String>> dataKtg, KategoriActivity kategoriActivity) {
+        this.dataKtg = dataKtg;
+        this.kategoriActivity = kategoriActivity;
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataKtg.size();
+    }
+
+    @Override
+    public void onBindViewHolder(HolderDataKtg holder, int position) {
+        HashMap<String, String> data = dataKtg.get(position);
+        holder.txKode.setText(data.get("kode_kategori"));
+        holder.txNama.setText(data.get("nama_kategori"));
+
+        //begin new
+//        if (position % 2 == 0) {
+//            holder.cLayout1.setBackgroundColor(Color.rgb(230, 245, 240));
+//        } else {
+//            holder.cLayout1.setBackgroundColor(Color.rgb(255, 255, 245));
+//        }
+
+        holder.cLayout1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                kategoriActivity.edKodeKat.setText(data.get("kode_kategori"));
+                kategoriActivity.edNamaKategori.setText(data.get("nama_kategori"));
+            }
+        });
+    }
+
+    @Override
+    public HolderDataKtg onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_kategori, parent, false);
+        return new HolderDataKtg(v);
+    }
+
+    public void updateData(List<HashMap<String, String>> newData) {
+        dataKtg.clear();
+        dataKtg.addAll(newData);
+        notifyDataSetChanged();
+    }
+
+    public static class HolderDataKtg extends RecyclerView.ViewHolder {
+        TextView txKode, txNama;
+        ConstraintLayout cLayout1;
+
+        public HolderDataKtg(View itemView) {
+            super(itemView);
+            txKode = itemView.findViewById(R.id.txKode);
+            txNama = itemView.findViewById(R.id.txNama);
+            cLayout1 = itemView.findViewById(R.id.itemKat);
+        }
+    }
 }
--- app/src/main/java/com/maulida/kinanti/adminpeminjamanalat/ui/KategoriActivity.java
+++ app/src/main/java/com/maulida/kinanti/adminpeminjamanalat/ui/KategoriActivity.java
@@ -1,16 +1,205 @@
 package com.maulida.kinanti.adminpeminjamanalat.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.textfield.TextInputEditText;
 import com.maulida.kinanti.adminpeminjamanalat.R;
+import com.maulida.kinanti.adminpeminjamanalat.adapter.AdapterDataKategori;
 
-public class KategoriActivity extends AppCompatActivity {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class KategoriActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final String URL = "http://192.168.10.83/pinjambarang/show_data_kategori.php";
+    private static final String URL3 = "http://192.168.10.83/pinjambarang/query_IUD_kategori.php";
+
+    private static final int INSERT_MODE = 1;
+    private static final int UPDATE_MODE = 2;
+    private static final int DELETE_MODE = 3;
+
+    private RecyclerView listKategori;
+    private AdapterDataKategori ktgAdapter;
+    private ImageView btnInsert1, btnUpdate1, btnDelete1;
+    public TextInputEditText edKodeKat;
+    public TextInputEditText edNamaKategori;
+
+    private final Map<String, String> params = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kategori);
+
+        listKategori = findViewById(R.id.listKategori);
+        ktgAdapter = new AdapterDataKategori(new ArrayList<>(), this);
+        listKategori.setLayoutManager(new LinearLayoutManager(this));
+        listKategori.setAdapter(ktgAdapter);
+
+        btnInsert1 = findViewById(R.id.btnInsert1);
+        btnUpdate1 = findViewById(R.id.btnUpdate1);
+        btnDelete1 = findViewById(R.id.btnDelete1);
+        edKodeKat = findViewById(R.id.edKodeKat);
+        edNamaKategori = findViewById(R.id.edNamaKategori);
+
+        btnInsert1.setOnClickListener(this);
+        btnUpdate1.setOnClickListener(this);
+        btnDelete1.setOnClickListener(this);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.bthome);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.bthome) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.btkat) {
+                return true;
+            } else if (item.getItemId() == R.id.btbrg) {
+                startActivity(new Intent(getApplicationContext(), BarangActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.btdata) {
+                startActivity(new Intent(getApplicationContext(), PeminjamanActivity.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            }
+            return false;
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        showDataKat("");
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnInsert1) {
+            queryInsertUpdateDelete(INSERT_MODE);
+        } else if (v.getId() == R.id.btnUpdate1) {
+            queryInsertUpdateDelete(UPDATE_MODE);
+        } else if (v.getId() == R.id.btnDelete1) {
+            queryInsertUpdateDelete(DELETE_MODE);
+        }
+    }
+
+    private void showDataKat(String namaBrg) {
+        StringRequest request = new StringRequest(
+                Request.Method.POST, URL,
+                response -> {
+                    try {
+                        JSONArray jsonArray = new JSONArray(response);
+                        List<HashMap<String, String>> dataKategoriList = new ArrayList<>();
+
+                        for (int x = 0; x < jsonArray.length(); x++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(x);
+                            HashMap<String, String> kategori = new HashMap<>();
+                            kategori.put("kode_kategori", jsonObject.getString("kode_kategori"));
+                            kategori.put("nama_kategori", jsonObject.getString("nama_kategori"));
+                            dataKategoriList.add(kategori);
+                        }
+
+                        ktgAdapter.updateData(dataKategoriList); // Memperbarui data adapter
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Toast.makeText(this, "Tidak dapat terhubung ke server", Toast.LENGTH_LONG).show()
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                params.clear();
+                params.put("nama_kategori", namaBrg);
+                return params;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
+    }
+
+    private void queryInsertUpdateDelete(int mode) {
+        StringRequest request = new StringRequest(
+                Request.Method.POST, URL3,
+                response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        String errorCode = jsonObject.getString("kode");
+
+                        if ("000".equals(errorCode)) {
+                            // Success
+                            showDataKat("");
+
+                        } else {
+                            Toast.makeText(this, "Gagal melakukan Operasi", Toast.LENGTH_LONG).show();
+                        }
+
+                        switch (mode) {
+                            case INSERT_MODE:
+                                Toast.makeText(this, "Berhasil insert data", Toast.LENGTH_LONG).show();
+                                break;
+                            case UPDATE_MODE:
+                                Toast.makeText(this, "Berhasil update data", Toast.LENGTH_LONG).show();
+                                break;
+                            case DELETE_MODE:
+                                Toast.makeText(this, "Berhasil delete data", Toast.LENGTH_LONG).show();
+                                break;
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                },
+                error -> Toast.makeText(this, "Tidak dapat terhubung ke server", Toast.LENGTH_LONG).show()
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                params.clear();
+                switch (mode) {
+                    case INSERT_MODE:
+                        params.put("mode", "insert");
+                        break;
+                    case UPDATE_MODE:
+                        params.put("mode", "update");
+                        break;
+                    case DELETE_MODE:
+                        params.put("mode", "delete");
+                        break;
+                }
+
+                params.put("kode_kategori", edKodeKat.getText().toString());
+                params.put("nama_kategori", edNamaKategori.getText().toString());
+
+                return params;
+            }
+        };
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(request);
     }
 }
